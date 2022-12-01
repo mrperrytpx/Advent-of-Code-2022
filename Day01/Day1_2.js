2ed6db5f59fc32070bbb80e273a2f420a40846e3
mrperrytpx <tomislav.siprak1996@gmail.com>
2022-12-01 11:06:45 +0100

    Advent of code Day 1, part1  and part 2

--- Day01/Day1_2.js
+++ Day01/Day1_2.js
@@ -1,17 +1,21 @@
 const readFile = require("fs").readFileSync;
 const file = readFile(__dirname + "/input.txt", "utf-8").replace(/\r/g, "").split("\n");
 
-let highestCalories = 0;
 let currentCalories = 0;
+let summedCaloriesArray = [];
 
 for (let i = 0; i < file.length; i++) {
     currentCalories += +file[i];
     if (file[i] == "" || i === file.length - 1) {
-        if (currentCalories > highestCalories) {
-            highestCalories = currentCalories;
-        }
+        summedCaloriesArray.push(currentCalories);
         currentCalories = 0;
     }
 }
 
-console.log(highestCalories);
+top3Elves = elfArray.sort((a, b) => {
+    if (a > b) return -1;
+    if (a < b) return 1;
+    return 0;
+}).slice(0, 3).reduce((a, b) => a + b, 0);
+
+console.log(top3Elves);
