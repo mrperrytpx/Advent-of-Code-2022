d829574cd6a7b411e9bcedfe22f7b230c0d97cdd
mrperrytpx <tomislav.siprak1996@gmail.com>
2022-12-17 22:54:27 +0100

    Advent of Code 2022 Day 17 part 1 almost done

--- Day17/Day17_1.js
+++ Day17/Day17_1.js
@@ -39,56 +39,88 @@ const rocks = [
 
 const board = [
     [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 1, 1, 0, 0, 0],
 ];
 
-let currentTetromino = 4;
-let landedRocks = 0;
+// const board = new Array(100).fill(new Array(7).fill(0));
 
+let currRock = 0;
+let landedRocks = 0;
+let moves = 0;
+
+let run = 0;
 while (landedRocks <= 2022) {
 
-    let rock = rocks[currentTetromino % 5];
-    for (let i = rock.shape.length - 1; i >= 0; i--) {
-        const rockRow = rock.shape[i];
-        const newBoardRow = [0, 0, ...rockRow];
-        while (newBoardRow.length !== 7) newBoardRow.push(0);
-        board.unshift([...newBoardRow]);
+
+    let rock = rocks[currRock % 5];
+    const boardHeight = board.length;
+    while (board.length < boardHeight + 3) {
+        board.unshift([0, 0, 0, 0, 0, 0, 0]);
     }
 
-    rock.topLeft = {
-        row: 0,
-        col: 2,
-    };
-    rock.potentialTopLeft = {
-        row: rock.topLeft.row + 1,
-        col: rock.topLeft.col
-    };
+    let currRow = 0;
+    let currCol = 2;
 
-    let notLanded = true;
+    let notStopped = true;
+    while (notStopped) {
 
-    while (notLanded) {
-        console.log(rock);
-        for (let r = 0; r < rock.shape.length; r++) {
-            for (let c = 0; c < rock.shape[r].length; c++) {
-                if (rock.shape[r][c] !== 0) {
-                    if (r + rock.potentialTopLeft.row >= board.length) {
-                        notLanded = false;
-                        board[r + rock.topLeft.row][c + rock.topLeft.col] = rock.shape[r][c];
-                        break;
-                    } else if (!!board[r + rock.potentialTopLeft.row][rock.potentialTopLeft.col]
-                        && !!board[rock.potentialTopLeft.row][c + rock.potentialTopLeft.col]) {
-                        notLanded = false;
-                        board[r + rock.topLeft.row][c + rock.topLeft.col] = rock.shape[r][c];
-                        break;
-                    }
+
+        let direction = file.charAt(moves % file.length);
+        console.log(direction);
+
+
+        if (direction === ">") {
+            for (let r = 0; r < rock.shape.length; r++) {
+                if (board[r + currRow][currCol + rock.shape[r].lenght] !== 0 && currCol + rock.shape[r].length < board[0].length) {
+                    currCol += 1;
+                }
+            }
+        } else {
+            for (let r = 0; r < rock.shape.length; r++) {
+                if (board[r + currRow][currCol - 1] !== 0 || currCol - 1 >= 0) {
+                    currCol -= 1;
                 }
             }
         }
-        rock.topLeft = { ...rock.potentialTopLeft };
-        rock.potentialTopLeft = { row: rock.potentialTopLeft.row + 1, col: rock.potentialTopLeft.col };
+
+
+        for (let r = 0; r < rock.shape.length; r++) {
+            for (let c = 0; c < rock.shape[r].length; c++) {
+                if (r + currRow + 1 >= board.length) {
+                    for (let i = 0; i < rock.shape.length; i++) {
+                        for (let j = 0; j < rock.shape[i].length; j++) {
+                            if (rock.shape[i][j] === 1) board[i + currRow][j + currCol] = 1;
+                        }
+                    }
+
+                    notStopped = false;
+                    break;
+                }
+
+                if (board[r + currRow + 1][c + currCol] === 1) {
+                    // board[r + currRow + 1][currCol + 1] === 1 to fix L shaped tetrisoid ????
+                    for (let i = 0; i < rock.shape.length; i++) {
+                        for (let j = 0; j < rock.shape[i].length; j++) {
+                            if (rock.shape[i][j] === 1) {
+                                board[i + currRow][j + currCol] = 1;
+                            }
+                        }
+                    }
+                    notStopped = false;
+                    break;
+                }
+            }
+        }
+        moves++;
+        if (notStopped) currRow += 1;
     }
-    break;
+
+    console.table(board);
+    console.log("_______________________________________________________________");
+    landedRocks++;
+    currRock++;
+    run++;
+
+    if (run === 3) break;
 }
 
-console.table(board);
+console.log(moves);
