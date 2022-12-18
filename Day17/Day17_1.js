944ec2287a78385480dc8ce343f2e3c49283a8d9
mrperrytpx <tomislav.siprak1996@gmail.com>
2022-12-18 15:06:34 +0100

    Advent of Code Day 17 Part 1 almost done, 1 edge case remaining

--- Day17/Day17_1.js
+++ Day17/Day17_1.js
@@ -37,20 +37,18 @@ const rocks = [
     }
 ];
 
-const board = [
+let board = [
     [0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0],
 ];
 
-let currRock = 0;
 let landedRocks = 0;
 let moves = 0;
-
 let run = 0;
 while (landedRocks <= 2022) {
 
-    let rock = rocks[currRock % 5];
+    let rock = rocks[landedRocks % 5];
     rock.shape.forEach(_ => board.unshift([0, 0, 0, 0, 0, 0, 0]));
 
     let currRow = 0;
@@ -59,19 +57,15 @@ while (landedRocks <= 2022) {
     let notStopped = true;
     while (notStopped) {
 
-
         let direction = file.charAt(moves % file.length);
         // console.log(direction);
-
 
         if (direction === ">") {
             let canMove = true;
             for (let r = 0; r < rock.shape.length; r++) {
                 for (let c = rock.shape[r].length - 1; c >= 0; c--) {
                     // Skip empty cells
-                    if (rock.shape[r][c] === 0) {
-                        continue;
-                    }
+                    if (rock.shape[r][c] === 0) continue;
                     // Check if the cell is at the right edge of the board
                     if (currCol + rock.shape[r].length >= board[0].length) {
                         canMove = false;
@@ -83,28 +77,17 @@ while (landedRocks <= 2022) {
                         break;
                     }
                 }
-                if (!canMove) {
-                    break;
-                }
+                if (!canMove) break;
             }
-            if (canMove) {
-                currCol += 1;
-                // console.log("can move right");
-            } else {
-                // console.log("can't move right");
-            }
+            if (canMove) currCol += 1;
         }
-
-
 
         if (direction === "<") {
             let canMove = true;
             for (let r = 0; r < rock.shape.length; r++) {
                 for (let c = 0; c < rock.shape[r].length; c++) {
                     // Skip empty cells
-                    if (rock.shape[r][c] === 0) {
-                        continue;
-                    }
+                    if (rock.shape[r][c] === 0) continue;
                     // Check if the cell is at the left edge of the board
                     if (currCol - 1 < 0) {
                         canMove = false;
@@ -116,21 +99,15 @@ while (landedRocks <= 2022) {
                         break;
                     }
                 }
-                if (!canMove) {
-                    break;
-                }
+                if (!canMove) break;
             }
-            if (canMove) {
-                currCol -= 1;
-                // console.log("can move left");
-            } else {
-                // console.log("can't move left");
-            }
+            if (canMove) currCol -= 1;
         }
 
         for (let r = 0; r < rock.shape.length; r++) {
             for (let c = 0; c < rock.shape[r].length; c++) {
-                if (r + currRow + 1 >= board.length) {
+                if (r + currRow + 1 >= board.length && rock.shape[r][c] === 1) {
+
                     for (let i = 0; i < rock.shape.length; i++) {
                         for (let j = 0; j < rock.shape[i].length; j++) {
                             if (rock.shape[i][j] === 1) board[i + currRow][j + currCol] = 1;
@@ -140,37 +117,46 @@ while (landedRocks <= 2022) {
                     notStopped = false;
                     break;
                 }
-
-                if (board[r + currRow + 1][c + currCol] === 1) {
-                    for (let i = 0; i < rock.shape.length; i++) {
-                        for (let j = 0; j < rock.shape[i].length; j++) {
-                            if (rock.shape[i][j] === 1) {
-                                board[i + currRow][j + currCol] = 1;
+                if (rock.shape[r][c] === 1)
+                    if (board[r + currRow + 1][c + currCol]) {
+                        // console.log(board[r + currRow + 1].toString(), r + currRow);
+                        for (let i = 0; i < rock.shape.length; i++) {
+                            for (let j = 0; j < rock.shape[i].length; j++) {
+                                if (rock.shape[i][j] === 1) {
+                                    // console.log(i, j);
+                                    board[i + currRow][j + currCol] = 1;
+                                }
                             }
                         }
+                        notStopped = false;
+                        break;
                     }
-                    notStopped = false;
-                    break;
-                }
             }
         }
         moves++;
         if (notStopped) currRow += 1;
     }
-    // console.table(board);
-
-    // console.log("_______________________________________________________________");
     landedRocks++;
-    currRock++;
+    let height = board.length;
+    for (let i = 0; i < height; i++) {
+        if (board[i].some(x => x === 1)) {
+            for (let j = 3; j < i; j++) board.shift();
+            break;
+        }
+    }
+
     run++;
-    if (run === 4) break;
+    if (run === 22) break;
+
 }
 
-console.table(board);
-
-// for (let i = 0; i < board.length; i++) {
-//     if (board[i].includes(1)) {
-//         console.log(board[i], i);
-//         break;
+// let height = 0;
+// for (let row of board) {
+//     if (row.some(cell => cell === 1)) {
+//         height += 1;
 //     }
 // }
+
+// console.log(height);
+
+console.table(board);
